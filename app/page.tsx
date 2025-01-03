7589bbab84db0495a01315d8aecef239da49731d
ayoub.fedil <ayoub.fedil@tersea.com>
2025-01-03 23:29:17 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { ThemeSwitch } from "@/components/ThemeSwitch";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       <ThemeSwitch /><main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+       
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
