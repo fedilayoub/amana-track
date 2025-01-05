c72b6c4df6839dd025c654b3ab3b4fc6d41d3757
ayoub.fedil <ayoub.fedil@tersea.com>
2025-01-05 22:12:16 +0100

    added header component

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import { ThemeSwitch } from "./ThemeSwitch";
+import { Logo } from "./ui/logo";
+import Link from "next/link";
+export const Header = () => {
+  return (
+    <div className="container mx-auto px-24 pt-10 flex justify-between items-center">
+      <Link className="flex items-center gap-2" href="/">
+        <Logo />{" "}
+        <p className="hidden md:block text-black dark:text-white font-semibold font-[family-name:var(--font-geist-sans)]">
+          Amanti
+        </p>
+      </Link>
+      <ThemeSwitch />
+    </div>
+  );
+};
