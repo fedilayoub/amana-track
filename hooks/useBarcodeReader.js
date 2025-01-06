b9712c397b20d428918559984e272b32ff4eefe6
ayoub.fedil <ayoub.fedil@tersea.com>
2025-01-06 23:21:40 +0100

    added barcode reader hook

---
+++ hooks/useBarcodeReader.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import Quagga from 'quagga';
+
+export const useBarcodeReader = () => {
+  const [barcode, setBarcode] = useState(null);
+   const getBarcode = (data) => {
+    try {
+     Quagga.decodeSingle({
+      src: data,
+      numOfWorkers: 0,
+      inputStream: {
+        type: 'ImageStream',
+        target: document.querySelector('#interactive-overlay')
+      },
+      decoder: {
+        readers: [
+          'code_39_reader',
+        ]
+      },
+      locate: true,
+      debug: {
+        showCanvas: true,
+        showPatches: true,
+        showFoundPatches: true,
+        showSkeleton: true,
+        showLabels: true,
+        showPatchLabels: true,
+        showRemainingPatchLabels: true,
+        boxFromPatches: {
+          color: 'red',
+          lineWidth: 2
+        }
+      }
+    }, (result) => {
+      if (result.codeResult) {
+        console.log(result.codeResult.code);
+        setBarcode(result.codeResult.code);
+        return result.codeResult.code
+      } else {
+        console.log('No barcode found');
+        setBarcode(null);
+        return null
+        
+      }
+    });
+  } catch (error) {
+    console.error('Error initializing Quagga:', error);
+  }
+   }
+    
+
+
+  return { getBarcode };
+};
+
